9b6fb1450e3bc1c6519b0da5c9d05b02799127f8
kwakkyoleen <52497663+kwakkyoleen@users.noreply.github.com>
2023-12-22 05:35:14 +0900

    Update Eclipstic.cs

--- Assets/Scripts/Eclipstic.cs
+++ Assets/Scripts/Eclipstic.cs
@@ -3,20 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Eclipstic : MonoBehaviour
 {
     public bool ready = true;
+    public bool istest = false;
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
     public struct Test
     {
-        [MarshalAs(UnmanagedType.I4)]
-        public int material;
-        [MarshalAs(UnmanagedType.I4)]
-        public int speed;
-        [MarshalAs(UnmanagedType.I4)]
-        public int block;
+        [MarshalAs(UnmanagedType.I2)]
+        public short perrity;
+        [MarshalAs(UnmanagedType.I2)]
+        public short material;
+        [MarshalAs(UnmanagedType.I2)]
+        public short speed;
+        [MarshalAs(UnmanagedType.I2)]
+        public short block;
+        [MarshalAs(UnmanagedType.I2)]
+        public short  empty_bit;
     }
 
     public Test test;
@@ -36,7 +42,7 @@ public class Eclipstic : MonoBehaviour
 
     IEnumerator waitSec()
     {
-        yield return new WaitForSecondsRealtime(0.2f);
+        yield return new WaitForSecondsRealtime(0.12f);
         //Debug.Log("start corutine");
         ready = true;
     }
@@ -53,7 +59,10 @@ public class Eclipstic : MonoBehaviour
         test.material = 0;
         test.speed = 0;
         test.block = 0;
+        test.empty_bit = 0;
+        test.perrity = 302;
         ready = true;
+        istest = false;
     }
 
     // Update is called once per frame
@@ -63,10 +72,18 @@ public class Eclipstic : MonoBehaviour
         {
             ready = false;
 
-            test.material = textureController.texint;
-            test.speed = (int)(controller.velocity*40);
-            test.speed = (test.speed > 100) ? 100 : test.speed;
-            test.block = textureController.obs;
+            test.material = (short)textureController.texint;
+            test.speed = (short)(controller.velocity*40);
+            test.speed = (short)((test.speed > 100) ? 100 : test.speed);
+            test.block = (short)textureController.obs;
+
+            if (istest)
+            {
+                test.material = 3;
+                test.speed = 25;
+                test.block = 125;
+            }
+
             //Debug.Log(" texture : " + test.material + "speed : " + test.speed);
             //serialController.SendStruct(getMashalData());
             String tstr = Encoding.Default.GetString(getMashalData());
@@ -99,8 +116,20 @@ public class Eclipstic : MonoBehaviour
             else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
                 Debug.Log("Connection attempt failed or disconnection detected");
             else
+            {
                 Debug.Log("arduino : " + message + "  || unity's texture : " + test.material + " , speed : " + test.speed);
-            while (serialController.ReadSerialMessage() != null) ;
+                if(istest)
+                {
+                    string deb = "";
+                    foreach (char c in tstr)
+                    {
+                        deb += ((byte) c + ", ");
+                    }
+
+                    Debug.Log(deb);
+                }
+                while (serialController.ReadSerialMessage() != null) ;
+            }
             StartCoroutine(waitSec());
             //StartCoroutine((IEnumerator)WaitForIt());
             return;
