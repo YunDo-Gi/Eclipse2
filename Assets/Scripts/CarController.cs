64d26203c8481c4b7381acb454bac4899fe22039
Woo, Haeun <suaveh97@khu.ac.kr>
2023-12-21 18:34:51 +0900

    sound - 경적음 해결, 엔진음 첨가

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,30 +5,26 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // 도로 끝 도달 여부를 check하는 변수와 함수
+    //도로 끝 도달 여부를 check하는 뱐수와 함수
     public delegate void CarReachedEnd(GameObject car);
     public event CarReachedEnd OnCarReachedEnd;
 
     private float roadEndZ;
-    bool isPerson;
 
-    // 신호등과 물체 충돌 방지 처리를 위한 코드
-    public float stopDistance = 4f; // 물체와의 거리의 임계점
+    //신호등과 물체 충돌 방지 처리를 위한 코드
+    public float stopDistance = 4f; //물체와의 거리의 임계점
 
     public bool canMove = true;
     private TrafficLight trafficLight;
-    private AudioSource audioSource;
-    private GameObject character;
+
+    private AudioSource audioSource; // 오디오 소스 추가
 
     private void Start()
     {
         trafficLight = FindObjectOfType<TrafficLight>();
-        // 오디오 소스 가져오기  
+
+        // 오디오 소스 컴포넌트 초기화
         audioSource = GetComponent<AudioSource>();
-        audioSource.playOnAwake = false;
-        // 캐릭터: 태그가 "person"인 게임오브젝트 
-        character = GameObject.FindWithTag("Player");
-        isPerson = false;
     }
 
     public void Initialize(float endZ)
@@ -36,34 +32,24 @@ public class CarController : MonoBehaviour
         roadEndZ = endZ;
     }
 
-  
     void Update()
     {
         // 자동차가 도로 끝에 도달하면 이벤트 호출
-        // 자동차는 위와 아래 방향 2가지로 존재, roadEndZ 위치가 다르니 상황에 맞게 event 호출
-        if ((transform.position.z <= roadEndZ && roadEndZ < 0) || (transform.position.z >= roadEndZ && roadEndZ >= 0))
-
-        isPerson = false;
-        // ???????? ???? ???? ???????? ?????? ????
-        // ???????? ???? ???? ???? 2?????? ????, roadEndZ ?????? ?????? ?????? ???? event ????
         if ((transform.position.z <= roadEndZ && roadEndZ < 0) || (transform.position.z >= roadEndZ && roadEndZ >= 0))
         {
             OnCarReachedEnd?.Invoke(gameObject);
         }
 
-
         // 빨간불로 인해 멈춰야 하는 조건
-        // 원래는 crosswalk에 tag 달아서 ray로 거리 측정해서 인지하게 하려고 했는데, 잘 안 돼서 일단 차선마다 특정 위치를 멈춤 조건으로 설정함
         bool redFlag;
         if (roadEndZ < 0)
         {
-            redFlag = trafficLight.currentColor == TrafficLight.LightColor.Red && (transform.position.z <= 8 && transform.position.z >= 5);
+            redFlag = trafficLight.currentColor == TrafficLight.LightColor.Red && (transform.position.z <= 8 && transform.position.z >= 2);
         }
         else
         {
-            redFlag = trafficLight.currentColor == TrafficLight.LightColor.Red && (transform.position.z <= -1 && transform.position.z >= -4);
+            redFlag = trafficLight.currentColor == TrafficLight.LightColor.Red && (transform.position.z <= 2 && transform.position.z >= -4);
         }
-        
 
         // 자동차 움직임 가능 여부 검사
         if (canMove)
@@ -71,31 +57,42 @@ public class CarController : MonoBehaviour
             // 빨간불 혹은 충돌 물체가 존재하면 stop
             if (redFlag || IsNearObstacle())
             {
-                // 일단 차 멈춤 
                 StopCar();
+            }
 
-                // 사람과 가까이 있으면 경적 소리 on
-                if (IsNearObstacle())
-                {
-                    audioSource.playOnAwake = true;  
-                }
-            
-                // 신호등 red duration 얼마 안 남으면 경적 소리 on 
-                /*if (trafficLight.redDuration <= 3f)
-                {
-                    audioSource.playOnAwake = true;
-                }*/
+            // 사람과의 충돌 시 소리 재생 로직 추가
+            else if (IsNearObstacle("Player"))
+            {
+                audioSource.Play();
             }
         }
         else
         {
-            // 빨간불도 아니고 충돌할만한 물체도 없으면 move ---> ???
+            // 빨간불도 아니고 충돌할만한 물체도 없으면 move
             if (!(redFlag || IsNearObstacle()))
             {
                 MoveCar();
             }
         }
     }
+
+    bool IsNearObstacle(string tag = null)
+    {
+        // 자동차의 전방에 Ray를 쏘아 물체와의 충돌을 감지
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, stopDistance))
+        {
+            //  태그가 제공되지 않았거나 태그가 일치하면 true 반환
+            if (tag == null || hit.collider.CompareTag(tag))
+            {
+                return true;
+            }
+        }
+
+        // 충돌할 물체가 없거나 해당 태그가 아니면 false 반환
+        return false;
+    }
+
 
     void StopCar()
     {
@@ -105,24 +102,5 @@ public class CarController : MonoBehaviour
     void MoveCar()
     {
         canMove = true;
-    }
-
-
-    bool IsNearObstacle()
-    {
-        // 자동차의 전방에 Ray를 쏘아 물체와의 충돌을 감지 --> 사람과의 충돌을 감지
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, stopDistance))
-        {
-            // 장애물이 사람이면 true 반환
-            if (hit.collider.CompareTag("Player"))
-            {
-                isPerson = true;
-                return true;
-            }
-        }
-
-        // 충돌할 물체가 없으면 false 반환
-        return false;
     }
 }
