f0c132f7bfb1a231fcea0971d6501aa8f9a4337d
Woo, Haeun <suaveh97@khu.ac.kr>
2023-12-22 02:39:34 +0900

    신호등 소리 test

--- Assets/Scripts/TrafficLight.cs
+++ Assets/Scripts/TrafficLight.cs
@@ -6,17 +6,22 @@ public class TrafficLight : MonoBehaviour
 {
     public Material[] lightMaterials;
 
-    //?????? ?????? ????????
-    public float greenDuration = 10f;
-    public float redDuration = 7f;
-    public float yellowDuration = 2f;
+    // 각각의 신호등 지속시간 (차 기준)
+    public float greenDuration = 12f;
+    public float redDuration = 20f;
+    public float yellowDuration = 3f;
 
     private Renderer trafficLightRenderer;
     private float timer = 0f;
 
+    private AudioSource audioRedLight1;
+    private AudioSource audioGreenLight;
+    private AudioSource audioRedLight2;
 
+    // 버튼이 눌렸을 때 true로 바꿔줌 
+    public bool isButton = false; 
 
-    //???? ?????? ??(????????)
+    // 현재 신호등 색 (도로기준) 
     public enum LightColor
     {
         Red,
@@ -24,7 +29,7 @@ public class TrafficLight : MonoBehaviour
         Green
     }
 
-    //???????? green???? ????
+    // 시작값을 green으로 설정 
     public LightColor currentColor = LightColor.Green;
     int isRed = 0, isYellow = 0, isGreen = 1;
 
@@ -32,11 +37,18 @@ public class TrafficLight : MonoBehaviour
     {
         trafficLightRenderer = GetComponent<Renderer>();
         SetMaterials();
+
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        audioRedLight1 = audioSources[0];
+        audioGreenLight = audioSources[1];
+        audioRedLight2 = audioSources[2];
+
+        isButton = true;
     }
 
     void SetMaterials()
     {
-        //currnetColor?? ?????? ?? rendering
+        //currnetColor에 맞춰서  rendering
         switch (currentColor)
         {
             case LightColor.Red:
@@ -65,24 +77,61 @@ public class TrafficLight : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        //???? -> ???? -> ???? -> ???????? ????
+        if (isButton)
+        {
+            if (currentColor == LightColor.Green)
+            {
+                audioGreenLight.Play();
+            }
+            if (currentColor == LightColor.Red && timer <= 10)
+            {
+                audioRedLight1.Play();
+            }
+            if (currentColor == LightColor.Red && timer > 10)
+            {
+                audioRedLight2.Play();
+            }
+        }
+
+
+
+
+        // 자동차 기준 : 적색 -> 초록색 -> 황색 -> 적색으로 변화
+        // 사람 기준: 녹색 -> 빨간색
         if (currentColor == LightColor.Red && timer >= redDuration)
         {
             currentColor = LightColor.Green;
             SetMaterials();
             timer = 0f;
+            if (isButton)
+            {
+                isButton = false;
+            }
+            // audioRedLight.Play();
         }
         else if (currentColor == LightColor.Green && timer >= greenDuration)
         {
             currentColor = LightColor.Yellow;
             SetMaterials();
             timer = 0f;
+            // audioGreenLight.Play();
         }
         else if (currentColor == LightColor.Yellow && timer >= yellowDuration)
         {
             currentColor = LightColor.Red;
             SetMaterials();
             timer = 0f;
+        }
+    }
+
+    void ButtonDownListener()
+    {
+        // X버튼을 누르면
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            isButton = true;
+            // 여기에 음향신호기 넣으면 됨
+            Debug.Log("X버튼을 눌렀습니다.");
         }
     }
 }
